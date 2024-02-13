7f98ab83ae042c62473caa0672c29fd440fce373
Paransha Vyas <paransha.vyas@prod.hclpnp.com>
2024-02-13 18:14:58 +0530

    sending my project

---
+++ ApacheKafkaIntegration/Logical/ServiceComponent.scm/config.scm
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1707738898209"
+    createdUser="paransha.vyas" id="-2eb9f5c5:18d9d26e644:-7e72"
+    type="service_component_resource" updatedTimestamp="1707738898209"
+    updatedUser="paransha.vyas" version="1.1">
+    <loggingConfiguration debug="false" resultWriterLoggingLevel="FULL" writeStubEventsToDB="false"/>
+</editableResource>
